dc9df41effcd8ada4b4bc43c390b471e67849732
Alexander B <r3disrussian@gmail.com>
2018-11-25 00:39:52 +0800

    Added args support

--- src/args.js
+++ src/args.js
@@ -1,7 +1,9 @@
+const argv = require('yargs').argv
+
 module.exports = {
-  token: process.env.token,
-  roomId: process.env.roomId,
-  volume: process.env.volume,
-  showSongName: process.env.showSongName,
-  stream: process.env.stream
+  token: argv.token || process.env.token,
+  roomId: argv.roomId || process.env.roomId,
+  volume: argv.volume || process.env.volume,
+  showSongName: argv.showSongName || process.env.showSongName,
+  stream: argv.stream || process.env.stream
 }
